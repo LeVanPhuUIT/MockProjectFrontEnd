a550da5dfe73eae90eac51bc82fb62258c7b0fda
LeVanPhuUIT <phulevan1995@gmail.com>
2018-05-10 22:52:54 +0700

    add book management and publisher
    add book management and publisher

--- src/app/components/book-management/book-management.component.ts
+++ src/app/components/book-management/book-management.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { products } from '../../model/products';
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-management',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-management.component.scss']
 })
 export class BookManagementComponent implements OnInit {
+  public gridData: any[] = products;
+  public gridView: GridDataResult;
+  public pageSize = 10;
+  public skip = 0;
+  private data: Object[];
 
-  constructor() { }
+  constructor(public urlRouter: Router) {
 
+    console.log(this.gridData);
+    this.loadItems();
+  }
   ngOnInit() {
+
+  }
+  onButtonClick() {
+    console.log('click');
   }
 
+  public pageChange(event: PageChangeEvent): void {
+    this.skip = event.skip;
+    this.loadItems();
+  }
+
+  private loadItems(): void {
+    this.gridView = {
+      data: this.gridData.slice(this.skip, this.skip + this.pageSize),
+      total: this.gridData.length
+    };
+  }
+  private addBook() {
+    this.urlRouter.navigateByUrl('/add-book');
+  }
 }
