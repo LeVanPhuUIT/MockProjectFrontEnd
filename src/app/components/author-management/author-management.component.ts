ff825c8eae7c93c6fb40ecf57971fa7360141ee4
Le Van Phu (FA.G0) <PhuLV2@fsoft.com.vn>
2018-05-15 18:34:06 +0700

    add load data author and search

--- src/app/components/author-management/author-management.component.ts
+++ src/app/components/author-management/author-management.component.ts
@@ -2,27 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { products} from '../../model/products';
 import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
 import { Router } from '@angular/router';
+import { Author } from '../../model/author';
+import { AuthorService } from '../../services/author.service';
 
 @Component({
   selector: 'app-author-management',
   templateUrl: './author-management.component.html',
-  styleUrls: ['./author-management.component.scss']
+  styleUrls: ['./author-management.component.scss'],
+  providers: [AuthorService]
 })
 export class AuthorManagementComponent implements OnInit {
 
   public gridData: any[] = products;
   public gridView: GridDataResult;
   public pageSize = 10;
+  public currentPage =1;
+  public searchString = "";
   public skip = 0;
   private data: Object[];
+  public newAuthor: Author;
 
-  constructor(public urlRouter:Router) {
-    
-    console.log(this.gridData);
-    this.loadItems();
+  constructor(public urlRouter:Router, private authorService: AuthorService) {
+    this.newAuthor = new Author();    
   }
-  ngOnInit() {
 
+  ngOnInit() {
+    this.loadItems();
   }
   onButtonClick() {
     console.log('click');
@@ -30,17 +35,25 @@ export class AuthorManagementComponent implements OnInit {
 
   public pageChange(event: PageChangeEvent): void {
     this.skip = event.skip;
+    this.currentPage = this.skip/this.pageSize +1;
+    console.log(this.currentPage);
     this.loadItems();
   }
 
   private loadItems(): void {
-    this.gridView = {
-      data: this.gridData.slice(this.skip, this.skip + this.pageSize),
-      total: this.gridData.length
-    };
+    this.authorService.getPagingAuthor(this.currentPage, this.pageSize, this.searchString)
+      .then(x => {
+        this.gridView = {
+          data: x["author"],
+          total: x["total"],
+        };
+        console.log(x["author"]);
+      });
+    console.log('load data');
+
   }
-  private addBook(){
-    this.urlRouter.navigateByUrl('/add-book');
+  private search(){
+    this.loadItems();
   }
 
 }
--- src/app/services/author.service.ts
+++ src/app/services/author.service.ts
@@ -1,8 +1,104 @@
 import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Request, RequestMethod, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Author } from '../model/author';
 
 @Injectable()
 export class AuthorService {
 
-  constructor() { }
+  public authorList: Observable<Author[]>;
+  private _authorList: BehaviorSubject<Author[]>;
+  private baseUrl: string;
+  private dataStore: {
+    authorList: Author[];
+  };
+
+  constructor(private http: Http) {
+    this.baseUrl = 'http://localhost:18595/api/';
+    this.dataStore = { authorList: [] };
+    this._authorList = <BehaviorSubject<Author[]>>new BehaviorSubject([]);
+    this.authorList = this._authorList.asObservable();
+  }
+
+  public getPagingAuthor(currentPage: number, pageSize: number, searchString: string){
+    return this.http.get(`${this.baseUrl}Authors?currentPage=${currentPage}&pageSize=${pageSize}&searchString=${searchString}`)
+      .toPromise()
+      .then(res => res.json())
+  }
+  
+  public getAll() {
+    return this.http.get(`${this.baseUrl}Authors`)
+      .toPromise()
+      .then(res => res.json())
+      // .subscribe(data => {
+      //   this.dataStore.authorList = data;
+      //   this._authorList.next(Object.assign({}, this.dataStore).authorList);
+      // }, error => console.log('Could not load author.'));
+  }
+
+  public addauthor(newAuthor: Author) {
+    console.log('add author');
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json; charset=utf-8');
+    console.log('add author : ' + JSON.stringify(newAuthor));
+
+
+    return this.http.post(`${this.baseUrl}Authors/`, JSON.stringify(newAuthor), { headers: headers })
+    .toPromise().
+    then(res => res.json());
+      // .map(response => response.json()).subscribe(data => {
+      //   this.dataStore.authorList.push(data);
+      //   this._authorList.next(Object.assign({}, this.dataStore).authorList);
+      // }, error => console.log('Could not add author.'));
+  }
+
+
+  public addauthor1<T>(newAuthor: T) {
+    console.log('add author');
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json; charset=utf-8');
+    console.log('add author : ' + JSON.stringify(newAuthor));
+
+
+    return this.http.post(`${this.baseUrl}Authors/`, JSON.stringify(newAuthor), { headers: headers })
+    .toPromise().
+    then(res => res.json());
+      // .map(response => response.json()).subscribe(data => {
+      //   this.dataStore.authorList.push(data);
+      //   this._authorList.next(Object.assign({}, this.dataStore).authorList);
+      // }, error => console.log('Could not add author.'));
+  }
+
+  public updateauthor(oldAuthor: Author) {
+    console.log('updateauthor');
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json; charset=utf-8');
+    console.log('Update author : ' + JSON.stringify(oldAuthor));
+
+    console.log(oldAuthor.authorId);
+    this.http.put(`${this.baseUrl}Categories/${oldAuthor.authorId}`, JSON.stringify(oldAuthor), { headers: headers })
+      .map(response => response.json()).subscribe(data => {
+        this.dataStore.authorList.forEach((t, i) => {
+          if (t.authorId === data.id) { this.dataStore.authorList[i] = data; }
+        });
+      }, error => console.log('Could not update author.'));
+  };
+
+  public removeItem(authorId: number) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json; charset=utf-8');
+    console.log('removeItem:' + authorId);
+    this.http.delete(`${this.baseUrl}Categories/${authorId}`, { headers: headers }).subscribe(response => {
+      this.dataStore.authorList.forEach((t, i) => {
+        if (t.authorId === authorId) { this.dataStore.authorList.splice(i, 1); }
+      });
+
+      this._authorList.next(Object.assign({}, this.dataStore).authorList);
+    }, error => console.log('Could not delete author.'));
+  }
+
 
 }
